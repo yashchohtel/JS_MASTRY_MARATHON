4ed1a379fd3443c5c591e8adae5ab250956b9672
Yash Chohtel <yashchohtel@gmail.com>
2026-05-23 00:00:52 +0530

    vending machine input disable feater done

--- Week-2/9. The Vending Machine/solution.js
+++ Week-2/9. The Vending Machine/solution.js
@@ -1,5 +1,18 @@
-getItem = () => {
+let items = document.querySelectorAll('.item');
+console.log(items[0])
 
+items.forEach(item => {
 
+    let checkbox = item.querySelector('input[type="checkbox"]');
+    let quentityInput = item.querySelector('input[type="number"]');
 
-}
+    checkbox.addEventListener('change', () => {
+        if (checkbox.checked) {
+            quentityInput.disabled = false;
+        } else {
+            quentityInput.disabled = true;
+            quentityInput.value = ''; // Clear the input value when disabled
+        }
+    });
+
+});
