5ee57bc71b10d22edb1e3c881a43f4bc65e1f4ff
Yash Chohtel <yashchohtel@gmail.com>
2026-05-28 22:48:53 +0530

    table printer done

--- Week-3/1. Table Printer/solution.js
+++ Week-3/1. Table Printer/solution.js
@@ -2,15 +2,15 @@ printTable = () => {
 
     // select the result element to show result
     let result = document.querySelector("#result")
+    result.innerHTML = "";
 
     // get the number
     let tableNumInput = document.querySelector("#num").value;
     let tableNum = Number(tableNumInput)
 
     // loop the number to print table
-    for (let i = 1; i <= 10 ; i++) {
-        console.log(`${tableNum} x ${i} = ${tableNum*i}`);  
+    for (let i = 1; i <= 10; i++) {
+        result.innerHTML += `<p>${tableNum} x ${i} = ${tableNum * i}</p>`;
     }
-    
 
-} 
+}
