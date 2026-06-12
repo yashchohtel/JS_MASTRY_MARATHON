ef8e781e4eb6bd3d49ad687e7fa1d05e470c74a1
Yash Chohtel <yashchohtel@gmail.com>
2026-06-12 23:59:17 +0530

    bank account project done

--- Week-5/3. Bank Account/pseudoCode.md
+++ Week-5/3. Bank Account/pseudoCode.md
@@ -15,24 +15,127 @@ EDGES: Withdraw more than balance, deposit 0 or negative, withdraw 0
 
 INPUT :-
 
-• 
+• withdraw amount and deposit amount
 
 PROCESS :-
 
-• 
+• deposit(amount) — adds to balance, increments transactionCount
+• withdraw(amount) — subtracts from balance (don’t allow going below 0), increments transactionCount
+• showBalance() — prints owner name, current balance, total transactions
 
 OUTPUT :-
 
-•
+• withdraw amount & remaining amount 
+• deposit amount & amount 
+• amount avilable in the bank
 
 EDGE :-
-• 
+
+• Withdraw more than balance, deposit 0 or negative, withdraw 0 
 
 -------------------------
 
 PSEUDOCODE :-
 
+<!-- create a object to store user bank informaiton -->
+account = { 
+    owner: "Priya", 
+    balance: 5000, 
+    transactionCount: 0
+}
 
+<!-- create a function which show the bank balance -->
+showBankBalance = () => {
 
+    <!-- get the balance -->
+    let avilableBalance = account.balance;
 
+    <!-- display in ui -->
+    print - avilableBalance
 
+}
+
+<!-- ------------------ -->
+
+<!-- create a function to deposit money -->
+depositMoney = () => {
+
+    <!-- get deposit input value -->
+
+    <!-- validate for empty input -->
+    IF (input is empty){
+
+        print - "Please enter deposit amount"
+        return
+
+    }
+
+    <!-- validate for negative and zero -->
+    IF (amount <= 0){
+
+        print - "Amount must be greater than 0"
+        return
+
+    }
+
+    <!-- store previous balance -->
+    previousBalance = account.balance
+
+    <!-- deposit money -->
+    account.balance += amount
+
+    <!-- increment transaction count -->
+    account.transactionCount++
+
+    <!-- display result -->
+    print -
+
+    Previous Balance : previousBalance
+
+    Deposited Amount : amount
+
+    Current Balance : account.balance
+
+}
+
+<!-- function to withdraw money -->
+withdrawMoney = () => {
+
+    <!-- get withdraw amount -->
+
+    <!-- validate for empty input -->
+    IF (input is empty){
+
+        print - "Please enter withdraw amount"
+        return
+
+    }
+
+    <!-- validate for negative and zero -->
+    IF (amount <= 0){
+
+        print - "Amount must be greater than 0"
+        return
+
+    }
+
+    <!-- check sufficient balance -->
+    IF (amount > account.balance){
+
+        print - "Insufficient balance"
+        return
+
+    }
+
+    <!-- store previous balance -->
+    previousBalance = account.balance
+
+    <!-- withdraw money -->
+    account.balance -= amount
+
+    <!-- increment transaction count -->
+    account.transactionCount++
+
+    <!-- display result -->
+
+}
--- Week-5/3. Bank Account/solution.js
+++ Week-5/3. Bank Account/solution.js
@@ -1,26 +1,101 @@
-function findEvenOdd() {
+// create a object to store user bank informaiton
+const account = {
+    owner: "yash",
+    balance: 5000,
+    transactionCount: 0
+};
 
-    // Get the number 
-    let numInput = document.getElementById("num").value;
-    let num = Number(numInput);
-    let result = document.getElementById("result");
+// create a function which show the bank balance
+const showBankBalance = () => {
 
-    // empty input validation check
-    if(numInput === ""){
-        result.innerHTML = "❌ Please fill all fields";
-    } 
+    // get the balance
+    const availableBalance = account.balance;
 
-    // if number is zero 
-    else if (num === 0){
-        result.innerHTML = "zero is special";
+    // display in ui
+    document.getElementById("result").innerHTML = `<p>Available Balance : ₹${availableBalance}</p>`;
+
+};
+
+// a function to deposit money
+depositMoney = () => {
+
+    const depositAmountInput = document.getElementById("depositAmount").value;
+    const depositAmount = Number(depositAmountInput);
+
+    // result element
+    const result = document.getElementById("result");
+
+    if (!depositAmountInput) {
+        result.innerHTML = "Please enter deposit amount";
+        return;
     }
 
-    // odd even check
-    else if (num % 2 === 0){
-        result.innerHTML = `${num} is even number`;
-    } else{
-        result.innerHTML = `${num} is odd number`;
+    if (depositAmount <= 0) {
+        result.innerHTML = "Amount must be greater than 0";
+        return;
     }
 
+    // store previous balance
+    const previousBalance = account.balance;
+
+    // deposit amount
+    account.balance += depositAmount;
+
+    // increment transactionCount
+    account.transactionCount++;
+
+    // display result
+    result.innerHTML = `
+    <p> <span>Previous Balance :</span> ₹${previousBalance}</p>
+    <p> <span>Deposited Amount :</span> ₹${depositAmount}</p>
+    <p> <span>Current Balance :</span> ₹${account.balance}</p>
+    <p> <span>Transaction Count :</span> ${account.transactionCount}</p>`;
+
+    // clear input
+    document.getElementById("depositAmount").value = ""
+
+}
+
+// a function to withdraw money
+withdrawMoney = () => {
+
+    // get input value
+    const withdrawAmountInput = document.getElementById("withdrawAmount").value;
+    const withdrawAmount = Number(withdrawAmountInput);
+
+    // result element
+    const result = document.getElementById("result");
+
+    if (!withdrawAmountInput) {
+        result.innerHTML = "Please enter withdraw amount";
+        return;
+    }
+
+    if (withdrawAmount <= 0) {
+        result.innerHTML = "Amount must be greater than 0";
+        return;
+    }
+
+    if (withdrawAmount > account.balance) {
+        result.innerHTML = "Insufficient balance";
+        document.getElementById("withdrawAmount").value = ""
+        return;
+    }
+
+    const previousBalance = account.balance;
+
+    account.balance -= withdrawAmount;
+
+    account.transactionCount++;
+
+    result.innerHTML = `
+        <p><span>Previous Balance :</span> ₹${previousBalance}</p>
+        <p><span>Withdraw Amount :</span> ₹${withdrawAmount}</p>
+        <p><span>Current Balance :</span> ₹${account.balance}</p>
+        <p><span>Transaction Count :</span> ${account.transactionCount}</p>
+    `;
+
+    // clear input
+    document.getElementById("withdrawAmount").value = ""
 
 }
